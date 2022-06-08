7041e649a9422541950b1b91c82d4cdd93a40f61
Arefin <arefin.iiuc.cse@gmail.com>
2022-06-08 21:49:59 +0600

    Added Get & Post Method in View(MVC)

---
+++ SMECommerce.App(View)/SMECommerce.App/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMECommerce.App.Models.CategoryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMECommerce.App.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public string Index()
+        {
+            return "This is the default Controller";
+        }
+
+        public IActionResult Create(CategoryCreate categoryCreate)
+        {
+            if( categoryCreate.Name !=null)
+            {
+                return View("Success");
+            }
+            return View();
+        }
+        public string CategoryListCreate(CategoryCreate[] categories)
+        {
+            string data = "Category List Create" + Environment.NewLine;
+            if (categories != null && categories.Any())
+            {
+                foreach (var category in categories)
+                {
+                    data += $"Category Create: {category.Name} Code: {category.Code}" + Environment.NewLine;
+                }
+            }
+
+            return data;
+        }
+    }
+}
